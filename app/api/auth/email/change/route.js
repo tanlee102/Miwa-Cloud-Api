cf397a151d2d7d4e51c40bacfdf5ead5ef05e726
LE QUOC TAN <letanprox@gmail.com>
2024-07-15 10:32:11 +0700

    T

---
+++ app/api/auth/email/change/route.js
@@ -0,0 +1,57 @@
+import { NextResponse } from 'next/server';
+import dotenv from 'dotenv';
+import jwt from 'jsonwebtoken';
+import axios from 'axios';
+
+export const dynamic = 'force-dynamic';
+
+dotenv.config();
+
+export async function GET(request) {
+  try {
+
+    const url = new URL(request.url);
+    const token = url.searchParams.get('token');
+
+    const API_KEY = process.env.API_KEY; // Replace with your actual API key
+    const JWT_KEY = process.env.JWT_SECRET; // Replace with your JWT secret
+    const API_URL = 'http://localhost:8080/api/v1/auth/email/verify'; // Replace with your actual backend API endpoint
+
+    try {
+
+        const decoded = jwt.verify(token, JWT_KEY);
+        const {email} = decoded;
+
+        const response = await axios.post(API_URL, {
+                "email": email,
+            }, {
+                headers: {
+                    'API-KEY': API_KEY
+                }
+            });
+
+              // Create a new response with a redirect
+      const redirectResponse = NextResponse.redirect('http://localhost:3001');
+
+      // Set the cookies in the redirect response
+      const cookieOptions = {
+        // httpOnly: true,
+        // secure: process.env.NODE_ENV === 'production',
+        path: '/',
+        // sameSite: 'strict'
+        expires: new Date(Date.now() + 1 * 24 * 60 * 60 * 1000) // 7 days from now
+      };
+
+      redirectResponse.cookies.set('token', response.data.token, cookieOptions);
+
+      return redirectResponse;
+    
+    } catch (error) {
+        console.log(error)
+        return new NextResponse('Invalid token', { status: 401 });
+    }
+
+  } catch (error) {
+    return NextResponse.json({ error: 'Error handling callback.' }, { status: 400 });
+  }
+}
---
+++ app/api/auth/email/verify/route.js
@@ -0,0 +1,60 @@
+import { NextResponse } from 'next/server';
+import dotenv from 'dotenv';
+import jwt from 'jsonwebtoken';
+import axios from 'axios';
+
+export const dynamic = 'force-dynamic';
+
+dotenv.config();
+
+export async function POST(request) {
+  try {
+    // Parse the body of the request
+    const body = await request.json();
+
+    // Extract email and password from the body
+    const { email } = body;
+
+    console.log(email)
+
+    const API_KEY = process.env.API_KEY; // Replace with your actual API key
+    const JWT_KEY = process.env.JWT_SECRET; // Replace with your JWT secret
+    const API_URL = "http://localhost:3000/api/mail/send3";
+
+    try {
+
+      // Create JWT
+      const token = jwt.sign({ email }, JWT_KEY, { expiresIn: '1h' });
+      const verificationLink = `http://localhost:3000/api/auth/email/change?token=${token}`;
+
+      const htmlContent = `
+          <html>
+              <body>
+                  <p>Verify user:</p>
+                  <a href="${verificationLink}">Click here to verify your email</a>
+              </body>
+          </html>
+      `;
+  
+      const response = await axios.post(API_URL, {
+          email: email,
+          subject: "Verify",
+          text: htmlContent
+      }, {
+          headers: {
+              'API-KEY': API_KEY
+          }
+      });
+
+      // Return the response from the server
+      return NextResponse.json({success: "success"}, { status: 200 });
+
+    } catch (error) {
+    //   console.log(error)
+      return NextResponse.json({ error: 'Registration failed' }, { status: error.response.status });
+    }
+
+  } catch (error) {
+    return NextResponse.json({ error: 'Error handling callback.' }, { status: 400 });
+  }
+}
