3f2bd0ad465df324f7441ad240a61906ef6bfbdb
LE QUOC TAN <letanprox@gmail.com>
2024-08-18 16:39:08 +0700

    T

--- app/api/auth/google/callback/route.js
+++ app/api/auth/google/callback/route.js
@@ -81,7 +81,7 @@ export async function GET(request) {
         return redirectResponse;
 
       } catch (error) {
-        return NextResponse.json({ error: 'Registration failed' }, { status: error.response?.status });
+        return NextResponse.json({ error: error }, { status: error.response?.status });
       }
 
     }else{
